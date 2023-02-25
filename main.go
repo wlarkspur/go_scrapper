49f53c80b50842684de02fecf6abe0413d69d0d0
wlarkspur <newacon@naver.com>
2023-02-25 21:21:15 +0200

    start url checker

--- main.go
+++ main.go
@@ -1,33 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"learngo/mydict"
+	"net/http"
 )
 
-/* type person struct {
-	name    string
-	age     int
-	favFood []string
-} */
-
-/* func main() {
-	account := accounts.NewAccount("jack")
-	account.Deposit(10)
-
-	fmt.Println(account)
-}
-*/
+var errRequestFailed = errors.New("Request Failed")
 
 func main() {
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	fmt.Println(dictionary)
-	err := dictionary.Delete(baseWord)
-	if err != nil {
-		fmt.Println(err)
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	word, _ := dictionary.Search(baseWord)
-	fmt.Println(word)
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitUrl(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitUrl(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	fmt.Println(err, resp.StatusCode)
+	return nil
 }
