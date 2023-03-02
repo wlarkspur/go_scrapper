42b5852c5bafb902a4c44cf9d2cb1cedec55db27
wlarkspur <newacon@naver.com>
2023-03-02 20:50:20 +0200

    Go scrapper 80%

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -27,7 +29,29 @@ func main() {
 		extractedJob := getPage(i)
 		jobs = append(jobs, extractedJob...)
 	}
-	fmt.Println(jobs)
+	writeJobs(jobs)
+	fmt.Println("Done, extracted", len(jobs))
+}
+
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+	uft8bom := []byte{0xEF, 0xBB, 0xBF}
+	file.Write(uft8bom)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"Link", "Company", "Title", "Exp", "Edu"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{`https://www.jobkorea.co.kr/Recruit/GI_Read/` + job.id + `?Oem_Code=C1&logpath=1&stext=` + "러시아" + `&listno=`, job.company, job.title, job.exp, job.edu}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
 }
 
 func getPage(page int) []extractedJob {
@@ -54,11 +78,12 @@ func getPage(page int) []extractedJob {
 }
 
 func extractJob(card *goquery.Selection) extractedJob {
-	id, _ := card.Attr("data-gino")
+	id, _ := card.Attr("data-gno")
 	company := cleanString(card.Find(".post-list-corp>a").Text())
 	title := cleanString(card.Find(".post-list-info>a").Text())
 	exp := cleanString(card.Find(".option .exp").Text())
 	edu := cleanString(card.Find(".option .edu").Text())
+
 	return extractedJob{
 		id:      id,
 		company: company,
